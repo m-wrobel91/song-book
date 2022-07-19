8095a19115a9272168373f6da8bf8a4094aa676d
m-wrobel91 <wrobel.michal91@gmail.com>
2022-07-19 21:43:55 +0200

    add new song validation

---
+++ src/main/java/pl/mwrobel91/songbook/validator/NewSongValidator.java
@@ -0,0 +1,26 @@
+package pl.mwrobel91.songbook.validator;
+
+import org.apache.commons.lang3.EnumUtils;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import pl.mwrobel91.songbook.dto.SongDTO;
+import pl.mwrobel91.songbook.enums.Category;
+
+public class NewSongValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return SongDTO.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        SongDTO songDTO = (SongDTO) target;
+        checkIfCategoryIsProper(songDTO.getCategory(), errors);
+    }
+
+    private void checkIfCategoryIsProper(String category, Errors errors) {
+        if(!EnumUtils.isValidEnum(Category.class, category)) {
+            errors.rejectValue("category", "Given category string is not within enum values");
+        }
+    }
+}
