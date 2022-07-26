212d2723e2cd7b8d69dced7717bc81615037d55b
m-wrobel91 <wrobel.michal91@gmail.com>
2022-07-26 21:16:00 +0200

    - add testing of song.title field validation

--- src/main/java/pl/mwrobel91/songbook/validator/NewSongValidator.java
+++ src/main/java/pl/mwrobel91/songbook/validator/NewSongValidator.java
@@ -1,6 +1,7 @@
 package pl.mwrobel91.songbook.validator;
 
 import org.apache.commons.lang3.EnumUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -20,13 +21,27 @@ public class NewSongValidator implements Validator {
     public void validate(Object target, Errors errors) {
         SongDTO songDTO = (SongDTO) target;
         checkIfCategoryIsProper(songDTO.getCategory(), errors);
+        checkIfTitleEmptyOrNUll(songDTO.getTitle(), errors);
+        checkIfLyricsEmptyOrNUll(songDTO.getLyrics(), errors);
     }
 
-    private void checkIfCategoryIsProper(String category, Errors errors) {
+    private void checkIfCategoryIsProper(final String category, Errors errors) {
         if (!EnumUtils.isValidEnum(Category.class, category)) {
             final String errorCode = MessageFormat.format(
                     "Given category: <{0}> is not within enum values of: <{1}>", category, Category.class);
             errors.rejectValue("category", errorCode);
+        }
+    }
+    private void checkIfTitleEmptyOrNUll(final String title, Errors errors) {
+        if (StringUtils.isBlank(title)) {
+            final String errorCode = "Title must not be empty nor null";
+            errors.rejectValue("title", errorCode);
+        }
+    }
+    private void checkIfLyricsEmptyOrNUll(final String title, Errors errors) {
+        if (StringUtils.isBlank(title)) {
+            final String errorCode = "Lyrics must not be empty nor null";
+            errors.rejectValue("lyrics", errorCode);
         }
     }
 }
